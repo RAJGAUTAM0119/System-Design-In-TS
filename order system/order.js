65f20b3f9cf6d3ec3cb98684b068bebdf693c7ff
Raj Gautam <rajgautam0119@gmail.com>
2026-09-08 19:05:50 +0530

    practice project

--- order system/order.js
+++ order system/order.js
@@ -1,4 +1,56 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("order");
+class Customer {
+    id;
+    name;
+    email;
+    constructor(id, name, email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+}
+class Order {
+    orderId;
+    customer;
+    items;
+    constructor(orderId, customer, items) {
+        this.orderId = orderId;
+        this.customer = customer;
+        this.items = items;
+    }
+    addItems() {
+        return;
+    }
+    removeItems() {
+        return;
+    }
+    getTotal() {
+        return;
+    }
+}
+class OrderItem {
+    product;
+    quantity;
+    constructor(product, quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+    itemTotal() {
+        return this.product.price * this.quantity;
+    }
+}
+class Product {
+    name;
+    id;
+    price;
+    constructor(name, id, price) {
+        this.name = name;
+        this.id = id;
+        this.price = price;
+    }
+}
+const product1 = new Product("Keyboard", 2015, 1250);
+const orderItem1 = new OrderItem(product1, 10);
+console.log(orderItem1.itemTotal());
 //# sourceMappingURL=order.js.map
--- order system/order.ts
+++ order system/order.ts
@@ -1,1 +1,38 @@
-console.log("order")
+class Customer {
+  constructor(public id: number, public name: string, public email: string) { }
+}
+
+
+class Order {
+  constructor(public orderId: number, public customer: string, public items: number) { }
+
+  addItems() {
+    return
+  }
+  removeItems() {
+    return
+  }
+  getTotal() {
+    return
+  }
+}
+
+
+class OrderItem {
+  constructor(public product: Product, public quantity: number) { }
+
+  itemTotal() {
+    return this.product.price * this.quantity
+  }
+}
+
+
+class Product {
+  constructor(public name: string, public id: number, public price: number) { }
+}
+
+
+const product1 = new Product("Keyboard", 2015, 1250)
+
+const orderItem1 = new OrderItem(product1, 10)
+console.log(orderItem1.itemTotal())
